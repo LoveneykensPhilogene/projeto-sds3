6ff0b15a9aa78280cb6ace8c1bd744ee5fe982b4
Loveneykens Philogene <loveneykens@gmail.com>
2021-05-05 23:32:10 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/services/SellerService.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dsvendas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.entities.Seller;
+import com.devsuperior.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	public List<SellerDTO> findAll(){
+		List<Seller> resultSeller=sellerRepository.findAll();
+		return resultSeller.stream()
+				.map(mapSeller->new SellerDTO(mapSeller))
+				.collect(Collectors.toList());
+		
+	}
+	
+
+}
